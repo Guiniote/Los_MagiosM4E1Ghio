c5722369e6b68efc2e6291cdfe8817d10b398381
Guiniote <guiniote@gmail.com>
2021-04-05 20:01:55 -0300

    Otro intento de arreglo para el deploy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const puerto = process.env.port;
+const puerto = process.env.PORT;
 
 app.use(express.static(path.join(__dirname, '/public')))
 
