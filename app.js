8d27d9bbf3e71abc23600992638c421e38de0aef
Guiniote <guiniote@gmail.com>
2021-04-05 15:26:47 -0300

    Temas M04C06

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
 app.listen (3000, () => {
-    console.log('Servidor levantado');
+    console.log('Servidor levantado en el puerto 3000');
 });
